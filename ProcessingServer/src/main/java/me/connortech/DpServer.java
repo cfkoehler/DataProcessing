ff24a8ce59fb0130a4277f0bc7351cdcf283e9f1
Connor Koehler <connor@connortech.me>
2022-11-27 11:46:12 -0500

    Inital Project Set up

---
+++ ProcessingServer/src/main/java/me/connortech/DpServer.java
@@ -0,0 +1,9 @@
+package me.connortech;
+
+import emissary.Emissary;
+
+public class DpServer extends Emissary {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
