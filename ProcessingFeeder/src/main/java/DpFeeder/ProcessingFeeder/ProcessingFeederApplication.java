7b8991542813a6acc700762e0415871eef58511d
Connor Koehler <connor@connortech.me>
2023-07-21 18:22:43 -0400

    All the changes

---
+++ ProcessingFeeder/src/main/java/DpFeeder/ProcessingFeeder/ProcessingFeederApplication.java
@@ -0,0 +1,13 @@
+package DpFeeder.ProcessingFeeder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessingFeederApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessingFeederApplication.class, args);
+	}
+
+}
